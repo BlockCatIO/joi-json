523a4f83ccc7f614c8e1dcbf13c82e4e5fbc0545
Ryan Smylski <ryan@blockcat.io>
2018-01-09 11:25:44 -0800

    Add missed files from last commit

--- test/lib/base.test.js
+++ test/lib/base.test.js
@@ -1,86 +1,76 @@
-'use strict';
+"use strict";
 
 /*jshint expr: true*/
 
-const BaseSchema = require( '../../lib/base' );
+const BaseSchema = require("../../lib/base");
 
-const expect = require( 'chai' ).expect;
+const expect = require("chai").expect;
 
-const sinon = require( 'sinon' );
+const sinon = require("sinon");
 
-describe( 'lib/base', function() {
+describe("lib/base", function() {
+  describe("BaseSchema", function() {
+    describe("constructor", function() {
+      it("normal operation", function() {
+        let instance = new BaseSchema("any");
 
-    describe( 'BaseSchema', function() {
-
-        describe( 'constructor', function() {
-
-            it( 'normal operation', function() {
-
-                let instance = new BaseSchema( 'any' );
-
-                expect( instance.engineFuncName ).to.equal( 'any' );
-            });
-        });
-
-        describe( '.parse', function() {
-
-            it( 'normal operation', function() {
-
-                let specialSchema = { };
-
-                specialSchema.noParam = sinon.stub().returns( specialSchema );
-                specialSchema.withParam = sinon.stub().returns( specialSchema );
-
-                let engine = {
-
-                    special: sinon.stub().returns( specialSchema )
-                };
-
-                let instance = new BaseSchema( 'special' );
-
-                let config = {
-
-                    noParam: null,
-                    withParam: 'param!'
-                };
-
-                let schema = instance.parse( config, engine );
-
-                expect( schema ).to.equal( specialSchema );
-
-                expect( engine.special.calledOnce ).to.be.true;
-                expect( engine.special.withArgs().calledOnce ).to.be.true;
-
-                expect( specialSchema.noParam.calledOnce ).to.be.true;
-                expect( specialSchema.noParam.withArgs().calledOnce ).to.be.true;
-
-                expect( specialSchema.withParam.calledOnce ).to.be.true;
-                expect( specialSchema.withParam.withArgs( 'param!' ).calledOnce ).to.be.true;
-            });
-
-            it( 'unknown method', function() {
-
-                let specialSchema = { };
-
-                let engine = {
-
-                    special: sinon.stub().returns( specialSchema )
-                };
-
-                let instance = new BaseSchema( 'special' );
-
-                let config = {
-
-                    special: true
-                };
-
-                let schema = instance.parse( config, engine );
-
-                expect( schema ).to.equal( specialSchema );
-
-                expect( engine.special.calledOnce ).to.be.true;
-                expect( engine.special.withArgs().calledOnce ).to.be.true;
-            });
-        });
+        expect(instance.engineFuncName).to.equal("any");
+      });
     });
+
+    describe(".parse", function() {
+      it("normal operation", function() {
+        let specialSchema = {};
+
+        specialSchema.noParam = sinon.stub().returns(specialSchema);
+        specialSchema.withParam = sinon.stub().returns(specialSchema);
+
+        let engine = {
+          special: sinon.stub().returns(specialSchema),
+        };
+
+        let instance = new BaseSchema("special");
+
+        let config = {
+          noParam: null,
+          withParam: "param!",
+        };
+
+        let schema = instance.parse(config, engine);
+
+        expect(schema).to.equal(specialSchema);
+
+        expect(engine.special.calledOnce).to.be.true;
+        expect(engine.special.withArgs().calledOnce).to.be.true;
+
+        expect(specialSchema.noParam.calledOnce).to.be.true;
+        expect(specialSchema.noParam.withArgs().calledOnce).to.be.true;
+
+        expect(specialSchema.withParam.calledOnce).to.be.true;
+        expect(specialSchema.withParam.withArgs("param!").calledOnce).to.be
+          .true;
+      });
+
+      it("unknown method", function() {
+        let specialSchema = {};
+
+        let engine = {
+          special: sinon.stub().returns(specialSchema),
+        };
+
+        let instance = new BaseSchema("special");
+
+        let config = {
+          special: true,
+        };
+
+        let schema = instance.parse(config, engine);
+
+        expect(schema).to.equal(specialSchema);
+
+        expect(engine.special.calledOnce).to.be.true;
+        expect(engine.special.withArgs().calledOnce).to.be.true;
+      });
+    });
+  });
 });
