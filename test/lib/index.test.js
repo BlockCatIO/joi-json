a9d8fd99d0372883933fbd23c0c6135bf65a12d4
Ryan Smylski <ryan@blockcat.io>
2018-01-09 11:19:24 -0800

    Allow for custom types
    - Added check if type is a function of the engine, if not a pre-defined type
    - Using BaseSchema to parse options for custom types
    - Added test for custom type with joi engine
    - Adjusted tabbing for all lib files and test files
    - Updated to latest joi package-lock.json
    - Added eslint packages

--- test/lib/index.test.js
+++ test/lib/index.test.js
@@ -1,280 +1,305 @@
-'use strict';
+"use strict";
 
 /*jshint expr: true*/
 
-const expect = require( 'chai' ).expect;
+const expect = require("chai").expect;
 
-const joi = require( 'joi' );
+const joi = require("joi");
 
-const lov = require( 'lov' );
+const lov = require("lov");
 
-const requireBlocker = require( 'require-blocker' );
+const requireBlocker = require("require-blocker");
 
-const sinon = require( 'sinon' );
+const sinon = require("sinon");
 
-const freshy = require( 'freshy' );
+const freshy = require("freshy");
 
-const MODULE_PATH = '../../lib';
+const MODULE_PATH = "../../lib";
 
-describe( 'lib/index', function() {
+describe("lib/index", function() {
+  let index;
 
-    let index;
+  beforeEach(function() {
+    requireBlocker.reset();
 
-    beforeEach( function() {
+    index = require(MODULE_PATH);
+  });
 
-        requireBlocker.reset();
+  after(function() {
+    freshy.unload(MODULE_PATH);
+  });
 
-        index = require( MODULE_PATH );
+  describe(".builder", function() {
+    it("using defaults", function() {
+      let builder = index.builder();
 
+      expect(builder.constructor.name).to.equal("SchemaBuilder");
+      expect(builder.parser.engine).to.equal(joi);
     });
 
-    after( function() {
+    it("use lov when joi is not present", function() {
+      requireBlocker.block("joi");
 
-        freshy.unload( MODULE_PATH );
+      let builder = index.builder();
+
+      expect(builder.constructor.name).to.equal("SchemaBuilder");
+      expect(builder.parser.engine).to.equal(lov);
     });
 
-    describe( '.builder', function() {
+    it("use custom validation engine", function() {
+      let customEngine = {};
 
-        it( 'using defaults', function() {
+      let builder = index.builder(customEngine);
 
-            let builder = index.builder();
-
-            expect( builder.constructor.name ).to.equal( 'SchemaBuilder' );
-            expect( builder.parser.engine ).to.equal( joi );
-        });
-
-        it( 'use lov when joi is not present', function() {
-
-            requireBlocker.block( 'joi' );
-
-            let builder = index.builder();
-
-            expect( builder.constructor.name ).to.equal( 'SchemaBuilder' );
-            expect( builder.parser.engine ).to.equal( lov );
-        });
-
-        it( 'use custom validation engine', function() {
-
-            let customEngine = {};
-
-            let builder = index.builder( customEngine );
-
-            expect( builder.constructor.name ).to.equal( 'SchemaBuilder' );
-            expect( builder.parser.engine ).to.equal( customEngine );
-        });
-
-        it( 'fail: when engines do not exist', function() {
-
-            requireBlocker.block( 'joi', 'lov' );
-
-            expect( index.builder.bind( null ) ).to.throw( 'cannot find validation engine' );
-        });
+      expect(builder.constructor.name).to.equal("SchemaBuilder");
+      expect(builder.parser.engine).to.equal(customEngine);
     });
 
-    describe( 'SchemaBuilder', function() {
+    it("fail: when engines do not exist", function() {
+      requireBlocker.block("joi", "lov");
 
-        describe( 'build', function() {
+      expect(index.builder.bind(null)).to.throw(
+        "cannot find validation engine",
+      );
+    });
+  });
 
-            let builder;
+  describe("SchemaBuilder", function() {
+    describe("build", function() {
+      let builder;
 
-            let engine = {};
+      let engine = {};
 
-            let stringSchema;
+      let stringSchema;
 
-            beforeEach( function() {
+      beforeEach(function() {
+        stringSchema = {};
 
-                stringSchema = {};
+        stringSchema.required = sinon.stub().returns(stringSchema);
+        stringSchema.trim = sinon.stub().returns(stringSchema);
 
-                stringSchema.required = sinon.stub().returns( stringSchema );
-                stringSchema.trim =sinon.stub().returns( stringSchema );
+        builder = index.builder(engine);
 
-                builder = index.builder( engine );
+        engine.string = sinon.stub().returns(stringSchema);
+      });
 
-                engine.string = sinon.stub().returns( stringSchema );
-            });
+      it("single value schema", function() {
+        let schema = builder.build("string:required");
 
-            it( 'single value schema', function() {
+        expect(schema).to.equal(stringSchema);
 
-                let schema = builder.build( 'string:required' );
+        expect(stringSchema.required.calledOnce).to.be.true;
+      });
 
-                expect( schema ).to.equal( stringSchema );
-
-                expect( stringSchema.required.calledOnce ).to.be.true;
-            });
-
-            it( '@schema property', function() {
-
-                let schema = builder.build( {
-
-                        '@type': 'string',
-                        required: true,
-                        '@schema': true
-                    });
-
-                expect( schema ).to.equal( stringSchema );
-
-                expect( stringSchema.required.calledOnce ).to.be.true;
-            });
-
-            it( 'schema case', function() {
-
-                let schema = builder.build( {
-
-                        name: 'string:required'
-                    });
-
-                expect( schema ).to.eql( { name: stringSchema } );
-
-                expect( stringSchema.required.calledOnce ).to.be.true;
-            });
+      it("@schema property", function() {
+        let schema = builder.build({
+          "@type": "string",
+          required: true,
+          "@schema": true,
         });
+
+        expect(schema).to.equal(stringSchema);
+
+        expect(stringSchema.required.calledOnce).to.be.true;
+      });
+
+      it("schema case", function() {
+        let schema = builder.build({
+          name: "string:required",
+        });
+
+        expect(schema).to.eql({ name: stringSchema });
+
+        expect(stringSchema.required.calledOnce).to.be.true;
+      });
+    });
+  });
+
+  describe("Joi schema cases", function() {
+    let parser;
+
+    beforeEach(function() {
+      parser = index.parser(joi);
     });
 
-    describe( 'Joi schema cases', function() {
+    it("string", function() {
+      let schema = parser.parse("string: min=1, max=60, required");
 
-        let parser;
+      expect(schema.isJoi).to.be.true;
+      expect(schema._type).to.equal("string");
+      expect(schema._tests.length).to.equal(3);
 
-        beforeEach( function() {
+      expect(schema._tests[0].name).to.equal("min");
+      expect(schema._tests[0].arg).to.equal(1);
 
-            parser = index.parser( joi );
-        });
+      expect(schema._tests[1].name).to.equal("max");
+      expect(schema._tests[1].arg).to.equal(60);
 
-        it( 'string', function() {
+      expect(schema._tests[2].name).to.equal("trim");
 
-            let schema = parser.parse( 'string: min=1, max=60, required' );
-
-            expect( schema.isJoi ).to.be.true;
-            expect( schema._type ).to.equal( 'string' );
-            expect( schema._tests.length ).to.equal( 3 );
-
-            expect( schema._tests[0].name ).to.equal( 'min' );
-            expect( schema._tests[0].arg ).to.equal( 1 );
-
-            expect( schema._tests[1].name ).to.equal( 'max' );
-            expect( schema._tests[1].arg ).to.equal( 60 );
-
-            expect( schema._tests[2].name ).to.equal( 'trim' );
-
-            expect( schema._flags.presence ).to.equal( 'required' );
-            expect( schema._flags.trim ).to.be.true;
-        });
-
-        it( 'uuid', function() {
-
-            let schema = parser.parse( 'uuid:required' );
-
-            expect( schema.isJoi ).to.be.true;
-            expect( schema._type ).to.equal( 'string' );
-            expect( schema._tests.length ).to.equal( 2 );
-
-            expect( schema._tests[0].name ).to.equal( 'guid' );
-            expect( schema._tests[1].name ).to.equal( 'trim' );
-
-            expect( schema._flags.presence ).to.equal( 'required' );
-            expect( schema._flags.trim ).to.be.true;
-        });
-
-        it( 'email', function() {
-
-            let schema = parser.parse( 'email:required' );
-
-            expect( schema.isJoi ).to.be.true;
-            expect( schema._type ).to.equal( 'string' );
-            expect( schema._tests.length ).to.equal( 2 );
-
-            expect( schema._tests[0].name ).to.equal( 'email' );
-            expect( schema._tests[1].name ).to.equal( 'trim' );
-
-            expect( schema._flags.presence ).to.equal( 'required' );
-            expect( schema._flags.trim ).to.be.true;
-        });
-
-        it( 'number', function() {
-
-            let schema = parser.parse( 'number:min=1,max=10,required' );
-
-            expect( schema.isJoi ).to.be.true;
-            expect( schema._type ).to.equal( 'number' );
-            expect( schema._tests.length ).to.equal( 2 );
-
-            expect( schema._tests[0].name ).to.equal( 'min' );
-            expect( schema._tests[0].arg ).to.equal( 1 );
-
-            expect( schema._tests[1].name ).to.equal( 'max' );
-            expect( schema._tests[1].arg ).to.equal( 10 );
-
-            expect( schema._flags.presence ).to.equal( 'required' );
-        });
-
-        it( 'boolean', function() {
-
-            let schema = parser.parse( 'boolean:required' );
-
-            expect( schema.isJoi ).to.be.true;
-            expect( schema._type ).to.equal( 'boolean' );
-            expect( schema._tests.length ).to.equal( 0 );
-
-            expect( schema._flags.presence ).to.equal( 'required' );
-        });
-
-        it( 'object', function() {
-
-            let schema = parser.parse( {
-
-                name: 'string:required',
-                age: 'number'
-            });
-
-            expect( schema.isJoi ).to.be.true;
-            expect( schema._type ).to.equal( 'object' );
-            expect( schema._inner.children.length ).to.equal( 2 );
-
-            expect( schema._inner.children[0].key ).to.equal( 'name' );
-            expect( schema._inner.children[0].schema._type ).to.equal( 'string' );
-            expect( schema._inner.children[0].schema._flags.presence ).to.equal( 'required' );
-
-            expect( schema._inner.children[1].key ).to.equal( 'age' );
-            expect( schema._inner.children[1].schema._type ).to.equal( 'number' );
-            expect( schema._inner.children[1].schema._flags.presence ).to.not.exist;
-        });
-
-        it( 'array', function() {
-
-            let schema = parser.parse( {
-
-                '@items': 'string:required'
-            });
-
-            expect( schema.isJoi ).to.be.true;
-            expect( schema._type ).to.equal( 'array' );
-
-            expect( schema._inner.items.length ).to.equal( 1 );
-
-            expect( schema._inner.items[0]._type ).to.equal( 'string' );
-            expect( schema._inner.items[0]._flags.presence ).to.equal( 'required' );
-        });
-
-        it( 'alternatives []', function() {
-
-            let schema = parser.parse( [ 'uuid:required', 'number:required' ] );
-
-            expect( schema.isJoi ).to.be.true;
-            expect( schema._type ).to.equal( 'alternatives' );
-
-            expect( schema._inner.matches[0].schema._type ).to.equal( 'string' );
-            expect( schema._inner.matches[0].schema._flags.presence ).to.equal( 'required' );
-
-            expect( schema._inner.matches[1].schema._type ).to.equal( 'number' );
-            expect( schema._inner.matches[1].schema._flags.presence ).to.equal( 'required' );
-
-            let result1 = joi.validate( '3952469a-1b45-4d5e-825c-e25ec8b57cf6', schema );
-            expect( result1.error ).to.be.null;
-
-            let result2 = joi.validate( 42, schema );
-            expect( result2.error ).to.be.null;
-
-            let result3 = joi.validate( 'forty-two', schema );
-            expect( result3.error ).to.exist;
-        });
+      expect(schema._flags.presence).to.equal("required");
+      expect(schema._flags.trim).to.be.true;
     });
+
+    it("uuid", function() {
+      let schema = parser.parse("uuid:required");
+
+      expect(schema.isJoi).to.be.true;
+      expect(schema._type).to.equal("string");
+      expect(schema._tests.length).to.equal(2);
+
+      expect(schema._tests[0].name).to.equal("guid");
+      expect(schema._tests[1].name).to.equal("trim");
+
+      expect(schema._flags.presence).to.equal("required");
+      expect(schema._flags.trim).to.be.true;
+    });
+
+    it("email", function() {
+      let schema = parser.parse("email:required");
+
+      expect(schema.isJoi).to.be.true;
+      expect(schema._type).to.equal("string");
+      expect(schema._tests.length).to.equal(2);
+
+      expect(schema._tests[0].name).to.equal("email");
+      expect(schema._tests[1].name).to.equal("trim");
+
+      expect(schema._flags.presence).to.equal("required");
+      expect(schema._flags.trim).to.be.true;
+    });
+
+    it("number", function() {
+      let schema = parser.parse("number:min=1,max=10,required");
+
+      expect(schema.isJoi).to.be.true;
+      expect(schema._type).to.equal("number");
+      expect(schema._tests.length).to.equal(2);
+
+      expect(schema._tests[0].name).to.equal("min");
+      expect(schema._tests[0].arg).to.equal(1);
+
+      expect(schema._tests[1].name).to.equal("max");
+      expect(schema._tests[1].arg).to.equal(10);
+
+      expect(schema._flags.presence).to.equal("required");
+    });
+
+    it("boolean", function() {
+      let schema = parser.parse("boolean:required");
+
+      expect(schema.isJoi).to.be.true;
+      expect(schema._type).to.equal("boolean");
+      expect(schema._tests.length).to.equal(0);
+
+      expect(schema._flags.presence).to.equal("required");
+    });
+
+    it("object", function() {
+      let schema = parser.parse({
+        name: "string:required",
+        age: "number",
+      });
+
+      expect(schema.isJoi).to.be.true;
+      expect(schema._type).to.equal("object");
+      expect(schema._inner.children.length).to.equal(2);
+
+      expect(schema._inner.children[0].key).to.equal("name");
+      expect(schema._inner.children[0].schema._type).to.equal("string");
+      expect(schema._inner.children[0].schema._flags.presence).to.equal(
+        "required",
+      );
+
+      expect(schema._inner.children[1].key).to.equal("age");
+      expect(schema._inner.children[1].schema._type).to.equal("number");
+      expect(schema._inner.children[1].schema._flags.presence).to.not.exist;
+    });
+
+    it("array", function() {
+      let schema = parser.parse({
+        "@items": "string:required",
+      });
+
+      expect(schema.isJoi).to.be.true;
+      expect(schema._type).to.equal("array");
+
+      expect(schema._inner.items.length).to.equal(1);
+
+      expect(schema._inner.items[0]._type).to.equal("string");
+      expect(schema._inner.items[0]._flags.presence).to.equal("required");
+    });
+
+    it("alternatives []", function() {
+      let schema = parser.parse(["uuid:required", "number:required"]);
+
+      expect(schema.isJoi).to.be.true;
+      expect(schema._type).to.equal("alternatives");
+
+      expect(schema._inner.matches[0].schema._type).to.equal("string");
+      expect(schema._inner.matches[0].schema._flags.presence).to.equal(
+        "required",
+      );
+
+      expect(schema._inner.matches[1].schema._type).to.equal("number");
+      expect(schema._inner.matches[1].schema._flags.presence).to.equal(
+        "required",
+      );
+
+      let result1 = joi.validate(
+        "3952469a-1b45-4d5e-825c-e25ec8b57cf6",
+        schema,
+      );
+      expect(result1.error).to.be.null;
+
+      let result2 = joi.validate(42, schema);
+      expect(result2.error).to.be.null;
+
+      let result3 = joi.validate("forty-two", schema);
+      expect(result3.error).to.exist;
+    });
+  });
+
+  describe("Joi schema extended with custom type", function() {
+    let parser;
+
+    const customJoi = joi.extend(joi => ({
+      base: joi.string(),
+      name: "string20",
+      language: {
+        valid: "must be string with less than 20 characters",
+      },
+      rules: [
+        {
+          name: "valid",
+          description: "Valid String 20",
+          validate(params, value, state, options) {
+            if (value.length >= 20) {
+              return this.createError("string20.valid", {}, state, options);
+            }
+            return value;
+          },
+        },
+      ],
+    }));
+
+    beforeEach(function() {
+      parser = index.parser(customJoi);
+    });
+
+    it("custom type valid and required", function() {
+      const schema = parser.parse({
+        "@type": "string20",
+        // use null to call with no params.
+        valid: null,
+        required: true,
+      });
+      expect(schema.isJoi).to.be.true;
+      expect(schema._type).to.equal("string20");
+
+      expect(schema._tests[0].name).to.equal("valid");
+
+      expect(schema._flags.presence).to.equal("required");
+    });
+  });
 });
